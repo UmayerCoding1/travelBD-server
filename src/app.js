a84397d88e9e4e780aef72946a2b48074ae96a96
umayerCoding1 <umayerpro175@gmail.com>
2025-01-06 01:04:27 +0600

    check token

--- src/app.js
+++ src/app.js
@@ -4,13 +4,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "http://localhost:5174"
-    ],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json())
 app.use(express.static('../public/upload'))
 app.use(cookieParser())
