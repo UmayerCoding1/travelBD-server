dc00cff2b4dc78c7516253adfa7547ead83686a4
umayerCoding1 <umayerpro175@gmail.com>
2025-01-08 00:35:18 +0600

    check deploy

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,11 @@
 import {asyncHandler}  from '../utils/AsyncHandler.js';
+import {ApiResponse} from '../utils/ApiResponse.js'
 const registerUser = asyncHandler(async(req,res) => {
   console.log(req.body);
   
+  res.status(200).json(
+    new ApiResponse(200,req.body,'A new user is create')
+  )
 })
 
 export {
