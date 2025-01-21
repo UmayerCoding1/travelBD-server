1c9261d0d52b0ce93c212845a08615c5b95cc286
Umayer Hossain <umayerpro175@gmail.com>
2025-01-21 11:46:01 +0600

    Update cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,16 +1,12 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from 'fs';
 
+
 cloudinary.config({
-    cloud_name: 'dlh8axija',
-    api_key: '734479388384749',
-    api_secret: 'yDqsgyKi-rprpKUZnSesF-Z2_Xw'
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
-// cloudinary.config({
-//     cloud_name: process.env.CLOUDINARY_NAME,
-//     api_key: process.env.CLOUDINARY_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET
-// });
 
 
 
